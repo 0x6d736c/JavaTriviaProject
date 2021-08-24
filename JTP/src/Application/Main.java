c9ec4f6df34fc23b9be99d57ca9a89e0a39f0a71
Michael Liebegott <Michael.Liebegott@outlook.com>
2021-08-24 17:44:52 -0400

    Add prototype skeleton project.

---
+++ JTP/src/Application/Main.java
@@ -0,0 +1,11 @@
+package Application;
+
+public class Main {
+    /**
+     * The main class for the Java Trivia Project. Coordinates all of the consituent parts of the program:
+     * backend, GUI, etc. to provide a smooth experience.
+     */
+    public static void main(String[] args) {
+        // Drives the program. Or drives us insane. We'll see.
+    }
+}
